a812671c9014dcb2e6e9160d9e81a297c867e949
Lawrence Qupty <lawrencequp@gmail.com>
2023-05-24 19:25:20 -0700

    Icons and title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
-<script setup></script>
+<script></script>
 
 <template>
-  <div class="flex h-screen justify-items-end">
-    <div class="bg-primary-brown w-[32rem]">
-      <h1 class="text-8xl">Explore</h1>
+  <div class="flex h-screen justify-items-end text-white">
+    <div class="bg-primary-brown w-[30rem] z-10">
+      <div class="relative top-96 left-1/2 w-full">
+        <h1 class="font-didot text-[15rem] font-light">Explore</h1>
+        <span
+          class="font-lato text-2xl font-light relative -top-16 flex items-center"
+        >
+          <img
+            class="float-left mr-6"
+            src="../Coding Challenge Assets/Plus.svg"
+          />
+          More Details
+        </span>
+      </div>
     </div>
     <img
-      class="object-cover"
+      class="object-cover w-full"
       id="img-background"
       src="../Coding Challenge Assets/Coding-Challenge-Image.png"
     />
+  </div>
+  <div class="absolute bottom-0 right-0 mb-12 mr-8">
+    <img class="mb-6" src="../Coding Challenge Assets/Facebook.svg" />
+    <img class="" src="../Coding Challenge Assets/Instagram.svg" />
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        lato: "'Lato', sans-serif",
+        didot: "'GFS Didot', serif",
+      },
       colors: {
         'primary-brown':'#611818',
         'secondary-brown':'#A36754'
