9247db613e2ad6988ed8a6e1b5e63e64368ec0a1
Lawrence Qupty <lawrencequp@gmail.com>
2023-05-24 18:10:17 -0700

    Project init and background

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div class="flex h-screen justify-items-end">
+    <div class="bg-primary-brown w-[32rem]">
+      <h1 class="text-8xl">Explore</h1>
+    </div>
+    <img
+      class="object-cover"
+      id="img-background"
+      src="../Coding Challenge Assets/Coding-Challenge-Image.png"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary-brown':'#611818',
+        'secondary-brown':'#A36754'
+      },
+    },
+  },
+  plugins: [],
+}
+
